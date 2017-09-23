d43a76fbca8d690aea66af16e0c46b1608099870
JVJohnson <james.vinton.johnson@gmail.com>
2017-09-22 23:00:56 -0400

    finalized collision thanks to stack overflow

--- RRT/RRT_Touch.py
+++ RRT/RRT_Touch.py
@@ -13,37 +13,12 @@ COLOR_NODE = (0,0,0)
 COLOR_OBS = (255,0,0)
 
 
-MAP_WIDTH = 6000
+MAP_WIDTH = 4000
 MAP_HEIGHT = 4000
 START = [   
             
-            (   int(   0        )   ,   int(0           )   ),
-            (   int(MAP_WIDTH/2 )   ,   int(0           )   ),
-            (   int(MAP_WIDTH   )   ,   int(0           )   ),
-
             
-            (   int(MAP_WIDTH/4 )   ,   int(MAP_HEIGHT/6)   ), 
-            (3* int(MAP_WIDTH/4 )   ,   int(MAP_HEIGHT/6)   ),
-
-            (   int(   0        )   ,   int(MAP_HEIGHT/3)   ),
-            (   int(MAP_WIDTH/2 )   ,   int(MAP_HEIGHT/3)   ),
-            (   int(MAP_WIDTH   )   ,   int(MAP_HEIGHT/3)   ),
-
-            
-            (   int(MAP_WIDTH/4 )   ,   int(MAP_HEIGHT/2)   ), 
-            (3* int(MAP_WIDTH/4 )   ,   int(MAP_HEIGHT/2)   ),
-
-            (   int(   0        )   , 2*int(MAP_HEIGHT/3)   ),
-            (   int(MAP_WIDTH/2 )   , 2*int(MAP_HEIGHT/3)   ),
-            (   int(MAP_WIDTH   )   , 2*int(MAP_HEIGHT/3)   ),
-
-            
-            (   int(MAP_WIDTH/4 )   , 5*int(MAP_HEIGHT/6)   ), 
-            (3* int(MAP_WIDTH/4 )   , 5*int(MAP_HEIGHT/6)   ),
-            
-            (   int(   0        )   ,   int(MAP_HEIGHT  )   ),
-            (   int(MAP_WIDTH/2 )   ,   int(MAP_HEIGHT  )   ),
-            (   int(MAP_WIDTH   )   ,   int(MAP_HEIGHT  )   )
+            (   int(MAP_WIDTH/2   )   ,3*int(MAP_HEIGHT/4  )   )
 
             ]  
                 
@@ -51,23 +26,26 @@ START = [
 
 
 OBS =   [
-        ((400,600), (400,400), (600,400), (600,600))
+            (   (1500,1500),(1500,2500)     ),
+            (   (1500,1500),(2500,1500)     ),
+            (   (1500,2500),(2500,2500)     ),
+            (   (2500,1500),(2500,2500)     )
         ]
 
 
 
-MAX_NODES = 10000
-MOD = MAX_NODES / 100
+MAX_NODES = 1000
+MOD = MAX_NODES / 200
 
 
-LINE_THICKNESS = int( math.ceil((MAP_WIDTH + MAP_HEIGHT)/2000)   )
+LINE_THICKNESS = int( math.ceil((MAP_WIDTH + MAP_HEIGHT)/500)   )
 
 NODE_RADIUS =  LINE_THICKNESS +1
 
-EPSILON= int( (MAP_WIDTH + MAP_HEIGHT)/2     * .05)
+EPSILON= int( (MAP_WIDTH + MAP_HEIGHT)/2     * .15)
 
 
-def Draw(im, k, obs, redraw):
+def Draw(im, k, OBS, redraw):
     '''
     Draws the map
 
@@ -77,15 +55,15 @@ def Draw(im, k, obs, redraw):
 
     
     '''
+    for segment in OBS:
+        cv2.line(im, segment[0], segment[1], COLOR_OBS, thickness=LINE_THICKNESS+2)
     for node in redraw:
         cv2.line(im, node, redraw[node], COLOR_BG, thickness=LINE_THICKNESS)
     for node in k:  #draws all the nodes (not just new ones in case there is an update feature later)
         
         cv2.circle(im, node, NODE_RADIUS, COLOR_NODE)
         cv2.line(im, node, k[node], COLOR_EDGE, thickness=LINE_THICKNESS)
-    '''for obstacle in obs:
-        cv2.rectangle(im, obstacle[0], obstacle[1], COLOR_OBS, thickness = EPSILON) 
-    '''
+    
     
     cv2.imshow(TITLE, im)
     cv2.waitKey(10)
@@ -128,7 +106,7 @@ def distanceSortLambda(origin):
     '''
     return lambda x: distance(origin, x)
 
-def rewire(newNode, k, costs, im):
+def rewire(newNode, k, costs, im, OBS):
 
     '''
     rewires nearby nodes to a new node when generated
@@ -145,6 +123,7 @@ def rewire(newNode, k, costs, im):
     for node in k:
         if k[newNode] == node: continue
         if distance(node, newNode) > EPSILON: continue
+        if collides(node, newNode, OBS): continue
         
         newCost = costs[newNode] + distance(node, newNode)
         if newCost < costs[node]:
@@ -154,7 +133,48 @@ def rewire(newNode, k, costs, im):
             costs[node] = newCost
     return redraw        
 
+def ccw(A,B,C):
+    '''
+    Determines if the points are counter clock-wise
+        got this from stackoverflow.com/questions/3838329/how-can-i-check-if-two-line-segments-intersect
 
+    :param A: x,y point
+    :param B: another x,y point
+    :param C: one more x,y point
+
+    :return: boolean is counter clockwise or not
+    '''
+    First = (C[1] - A[1]) * (B[0] - A[0])
+    Second =(B[1] - A[1]) * (C[0] - A[0])
+
+    return First > Second
+
+def intersect(A,B,C,D):
+    '''
+    detects if two line segments intersect
+
+    :param A,B,C,D: x,y points of line segment AB and CD
+
+    :return: boolean if the lines intersect
+    '''
+    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
+    
+def collides(node, newNode, Obs):
+    '''
+    checks if two points intersect with the obstacles
+
+    :param node, newNode: nodes to check a segment bewteen
+    :param Obs: list of line segments to check
+
+    :return: boolean if it collides
+    '''
+    collides = False
+    for segment in Obs:
+        if intersect(node, newNode, segment[0], segment[1]):
+            collides = True
+            break
+    return collides
+    
     
 
 def main():
@@ -167,9 +187,12 @@ def main():
     Map = np.zeros((MAP_HEIGHT, MAP_WIDTH, 3))
     Map[:,:] = COLOR_BG
 
-    Obs = []
+
+    
+    
     k       = {}                #dict of nodes
     costs   = {}                #dict of costs
+    redraw  = {}                #dict of nodes to rewire
     for point in START:
         k[point]        = point
         costs[point]    = 0
@@ -203,25 +226,33 @@ def main():
                 parent = node
             
 
+
+
         #EpsCheck = 2*EPSILON / len(k)        #useful for an odd pattern
 
         #check to see if new node is just too far
         if distance(newNode, parent) > EPSILON:
-            
             continue
 
+        #checks to see if line collides
+        
+        if collides(parent, newNode, OBS):
+            continue
+
+
+        
         #add newNode into lists
         costs[newNode] = minCost
         k[newNode] = parent
 
-        redraw = rewire(newNode, k, costs, Map)
+        redraw  =  {**redraw, **rewire(newNode, k, costs, Map, OBS)   }
 
         
         #update image
-        #Draw(Map, k, Obs, redraw)
+        #Draw(Map, k, OBS, redraw)
         if(len(k) % MOD ==0):
             print("" + str(len(k)) + " / " + str(MAX_NODES) + "  (" + str(100.0*len(k)/MAX_NODES) + "%)")
-            Draw(Map, k, Obs, redraw)
+            Draw(Map, k, OBS, redraw)
         #if(len(k) % 50 ==0):
          #   cv2.imshow(TITLE, Map)
           #  cv2.waitKey(1)
@@ -229,7 +260,7 @@ def main():
         
     
     
-    Draw(Map, k, Obs, redraw)
+    Draw(Map, k, OBS, redraw)
     EndIm(Map, k)
 
 
