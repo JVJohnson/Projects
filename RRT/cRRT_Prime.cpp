68f52a8c487321a20ffda76598eccf8c23100041
JVJohnson <james.vinton.johnson@gmail.com>
2017-12-01 18:55:17 -0500

    Got RSR working. next step, Lsl and rsl

--- RRT/cRRT_Prime.cpp
+++ RRT/cRRT_Prime.cpp
@@ -14,6 +14,10 @@
 using namespace std;
 using namespace cv;
 
+//mathematical constants
+const double PI = atan(1)*4;
+
+
 //width & height of map
 const int mapW = 1000;
 const int mapH = 1000;
@@ -21,25 +25,30 @@ const int mapH = 1000;
 //initial node
 const int startX = 500;
 const int startY = 500;
-const double startTh = 0;
-
-const double stDeltX = 0;
+const double startTh = -PI+2;
 
 
 
+
+
+//basic car restraints
 const double carLength = 3;
-const double maxTheta = atan(1)/8;
-const double maxSpeed = 5;
+const double maxTheta = PI/32;
 
-const int DEF = 100;
+const double turnRad = carLength/tan(maxTheta);
+
+
+
 
 
 //max number of nodes
-const int nodeNum = 500;
+const int nodeNum = 10;
 
-
-const int nodeRad = 2;
+//Drawing Constants
+const int nodeRad = 4;
 const int lineThickness = 2;
+const int lineLen = ( (mapW + mapH)/2 ) /25;
+
 
 //Epsilon is a fraction of average of w and h
 const double EPSILON =  ( (mapW + mapH)/2 ) /7 ;
@@ -47,16 +56,12 @@ const double EPSILON =  ( (mapW + mapH)/2 ) /7 ;
 int currNodeNum = 0;
 
 
-//node leaf. contains current state and derivative
+//node leaf. contains current state and parent
 struct NodeLeaf
 	{
 		Point self;
 		double angle;
-
-		double speed;
-		double deltaAng;
-
-
+		double cost;
 
 		NodeLeaf* parent;
 	};
@@ -65,18 +70,11 @@ struct NodeLeaf
 void draw( NodeLeaf *nodes, Mat img)
 {
 
-	for(int i = 0; i<currNodeNum; i++ ) //iterates through each node
+	for(int i = 0; i<currNodeNum; i++ ) //iterates through each node and draws the pose direction
 	{
-		// cout << "Iteration: " << i <<  endl;
-		
-		// cout << "	suceeded" << endl;
+
 		circle(img, nodes[i].self, nodeRad, CV_RGB(0,0,0), nodeRad);
-		// cout << "	drew: " << nodes[i].self << endl;
-		// cout << "	node parent: ";
-		// cout << (nodes[i].parent) << endl;
-		
-		//line(img, nodes[i].self, nodes[i].parent->self, CV_RGB(0,0,0), lineThickness );
-		// cout << "	drew line" << endl;
+		arrowedLine(   img, nodes[i].self,    Point( nodes[i].self.x + lineLen*cos(nodes[i].angle) , nodes[i].self.y + lineLen*sin(nodes[i].angle) )   , CV_RGB(0,0,0), lineThickness, 8 );
 	}
 
 
@@ -86,34 +84,97 @@ void draw( NodeLeaf *nodes, Mat img)
 
 
 
-NodeLeaf drive(NodeLeaf init, Mat img)
-{
-	double us = init.speed;
 
-	double x = init.self.x;
-	double y = init.self.y;
-	double th = init.angle;
+
+
+//Right, straight, Right Dubins curve, returns length of the path
+
+//struct containing all the information needed to redraw the line
+struct DubinsResult
+{
+	int id = 0;
+	double dist;
+	Point Origin;
+	Point Final;
+	double alph;
+	double gamm;
+	double OriginLeave;
+	double FinalLeave;
+};
+
+DubinsResult RSR(NodeLeaf PoseOrigin, NodeLeaf PoseFinal)
+{
+	//creates the center of the turning radius circle on the right side of the origin pose
+	Point Origin;
+	Origin.x 	= PoseOrigin.self.x + turnRad*cos(PoseOrigin.angle + PI/2);
+	Origin.y 	= PoseOrigin.self.y + turnRad*sin(PoseOrigin.angle + PI/2);
+
+
+	//creates the center of the turning radius circle on the right side of the final pose
+	Point Final;
+	Final.x 	= PoseFinal.self.x  + turnRad*cos(PoseFinal.angle  + PI/2);
+	Final.y 	= PoseFinal.self.y  + turnRad*sin(PoseFinal.angle  + PI/2);
+
+	double deltX = Final.x-Origin.x;
+	double deltY = Final.y-Origin.y;
+
+	//Travel angle around the first circle
+	double OriginLeave = atan2(deltY, deltX);
+	double alph	= OriginLeave - PoseOrigin.angle + 2*PI;
+	while(alph > 2*PI) alph -= 2*PI;
+	while(alph < -2*PI) alph += 2*PI;
+
+	//straight distance to travel
+	double dist = sqrt(   (deltX)*(deltX) + (deltY)*(deltY)   );
+	
+	//Travel angle around the first circle
+	double FinalLeave = atan2(deltY, deltX);
+	double gamm	= FinalLeave - PoseFinal.angle -2*PI;
+	while (gamm > 2*PI) gamm-= 2*PI;
+	while (gamm < -2*PI) gamm+= 2*PI;
+	
+	// cout << gamm << endl;
+
+	DubinsResult result;
+	result.id 			= 0;
+	result.dist 		= abs(alph*turnRad)  +  abs(dist)  +  abs(gamm*turnRad);
+	result.Origin 		= Origin;
+	result.Final 		= Final;
+	result.alph 		= alph;
+	result.gamm 		= gamm;
+	result.OriginLeave 	= OriginLeave;
+	result.FinalLeave 	= FinalLeave;
+
+	return result;
 	
 
-	for(int step = 1; step < 20; step++)
-	{
-		x = x + us*cos(th);
-		y = y + us*sin(th);
-		th = th + us/carLength * tan(init.deltaAng);
-		circle(img, Point(x,y), 1, CV_RGB(0,0,255), 1);
-		
-		imshow("CaR-RT", img);
-		waitKey(4);
-		// cout << "drew: " << x << ", " << y << endl;
+}
 
-	};
-	NodeLeaf end;
-	end.self.x = x;
-	end.self.y = y;
-	end.angle  = th;
-	return end;
+//draws an RSR dubin's curve
+void drawRSR(DubinsResult r, NodeLeaf PoseOrigin, NodeLeaf PoseFinal,  Mat img)
+{
+	ellipse(img, Point(r.Origin.x, r.Origin.y), Size(turnRad, turnRad), (PoseOrigin.angle-PI/2) *180/PI, (r.alph) *180/PI, 0, Scalar(255,0,0), lineThickness, 8 );
+	ellipse(img, Point(r.Final.x, r.Final.y), Size(turnRad, turnRad), (PoseFinal.angle-PI/2)*180/PI, 0, (r.gamm)*180/PI, Scalar(255,0,0), lineThickness, 8 );
+
+	//circle(img, Point(Origin.x,Origin.y), turnRad , CV_RGB(0,0,255), lineThickness);
+	//circle(img, Point(Final.x,Final.y),turnRad , CV_RGB(0,0,255), lineThickness);
+
+	double OxLeft = -turnRad*cos(r.OriginLeave + PI/2) + r.Origin.x;
+	double OyLeft = -turnRad*sin(r.OriginLeave + PI/2) + r.Origin.y;
+	double FxLeft = -turnRad*cos(r.FinalLeave  + PI/2)  +r. Final.x ;
+	double FyLeft = -turnRad*sin(r.FinalLeave  + PI/2)  + r.Final.y ;
+
+	// cout << Point(OxLeft, OyLeft) << " to " << Point(FxLeft, FyLeft) << endl;
+	line(  img, Point(OxLeft, OyLeft), Point(FxLeft, FyLeft), CV_RGB(0,0,255), lineThickness );
+
 
 }
+
+
+
+
+
+
 
 
 
@@ -132,8 +193,9 @@ int main()
 	//setting up beginnning node
 	NodeLeaf start;
 	start.self = Point(startX, startY);
+	start.angle = startTh;
 	start.parent = &start;
-
+	start.cost = 0;
 
 	//add start node
 	nodes[currNodeNum] = start;
@@ -147,7 +209,6 @@ int main()
 
 
 
-
 	//iterates through node-making until desired number of nodes is reached
 	while(currNodeNum < nodeNum)
 	{
@@ -156,25 +217,34 @@ int main()
 		//creates new node with random position
 		
 		//____________TEST________________
-		//just a test version to see if the kinematic model works without having to implement non-holonmic regression for it
+		//just a test version to see if the dubins curves works without having to implement non-holonmic regression for it
 		
-		int randIndex = rand() % currNodeNum;
+		NodeLeaf newNode;
+		newNode.self.x = rand() % (mapW - 2*(int)(turnRad*2)) + 2*turnRad;
+		newNode.self.y = rand() % (mapH - 2*(int)(turnRad*2)) + 2*turnRad;
+		//returns random angle 
+		newNode.angle  = fmod(rand(), 2*PI);
 
-		NodeLeaf nextNode(nodes[randIndex]); 
-
-		//gets within range of (-DEF,DEF), then multilies by max theta to get (-maxTheta , maxTheta)
-		double dTheta = (    ( rand()%(2*DEF) )- DEF   )*1.0/DEF * maxTheta;
-				
-		//uses same  thing as above to get range (-maxSpeed, maxSpeed)
-		double dSpeed = (    ( rand()%(2*DEF) )- DEF   )*1.0/DEF * maxSpeed;
-		cout << "rand Th,S: " << dTheta << ", " << dSpeed << endl;
-
-		nextNode.speed = dSpeed;
-		nextNode.deltaAng = dTheta;
+		DubinsResult final = RSR(nodes[0], newNode);
+		DubinsResult next;
+		NodeLeaf chosen =nodes[0];
 
 
-		nextNode = drive(nextNode, img);
+		for(size_t i = 0; i < currNodeNum; i++)
+		{
+			next = RSR(nodes[i], newNode);
 
+			if(next.dist /*+ nodes[i].cost*/ < final.dist /*+ chosen.cost*/)
+			{
+				final = next;
+				chosen = nodes[i];
+			}
+		}
+
+		newNode.cost = final.dist + chosen.cost;
+		drawRSR(final, chosen, newNode, img);
+
+		// cout << "Length of path: " << len << endl;
 
 
 		//____________TEST___________________
@@ -220,7 +290,7 @@ int main()
 		// cout << "	with parent: " << newN.parent->self << endl;
 		
 		//add new node into the array
-		nodes[currNodeNum] = nextNode;
+		nodes[currNodeNum] = newNode;
 		currNodeNum++;
 
 	}
