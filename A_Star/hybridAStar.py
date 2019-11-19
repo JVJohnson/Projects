a839572e0942a2dd23d463b714426e0fe20187d2
JVJohnson <james.vinton.johnson@gmail.com>
2019-11-19 14:24:49 -0500

    finished node, now just need to run algorithm

--- A_Star/hybridAStar.py
+++ A_Star/hybridAStar.py
@@ -7,63 +7,34 @@ import math
 import time
 import csv
 import cProfile, pstats, io
-
-'''Pseudocode
-
-	Establish data structures
-		-Map: 2d array of integer spacing (numpy array)
-			-has freespace and/or occupiedSpace indicators(Pixel color)
-		-Frontier: priority queue
-		-Node: Int x, Int y, Double theta, Double realX, Double realY
-			   Node Parent
-
-	Iterate While:
-		-Frontier is NOT empty and
-		-Goal is NOT reached
-
-		Pick node from priority queue (sorted by heuristic)
-		check it is a valid node
-			-is it within bounds?
-			-is it in freespace?
-		Call Function: Find neighbors of current node
-		for each of the neighbors:
-			apply heuristic to neighbor
-			put neighbor in frontier queue according to heuristic 
+from hybridNode import HybridNode
 
 
 
-
-Function: Find Neighbors
-	Params: Parent Node
-	Optional Params: number of neighbors, max Turning angle, speed/distance
-
-	Utilizes the Max turning angle of our robot to spawn a certain number of
-	neighbor nodes
-
-	Take Parent realX, realY, and Theta
-	Apply kinematic equations for (numOfNeighbors) turning angles
-		- start at max negative steering angle
-		- end at max positive steering angle
-		- Good number is probably about 5-7 neighbors
-		Create Nodes At said positions (store realX, realY and Theta)
-
-
-'''	
 class HybridAStar:
 	def __init__(self, terrain, start, end):
 		self.terrain = terrain
 		self.start = start
 		self.end = end
-		self.speed = 100
-		self.radius = 10
-		self.definition = 10
+		self.speed = 10
+		self.radius = 20
+		self.definition = 160
 		self.num_neighbors = 7
+		self.admittance = 7
+		self.nodes = {}
+
+		self.nodes[start] = self.nodeCost(self.start)
+
+	def nodeCost(self, node):
+		return node.cost + self.end.distTo(node)
+
+
 
 
 if __name__ == "__main__":
 	dim = 1000
 	terrain = numpy.full((dim, dim, 3), 255)
-	start = (500, 800, 0)
-	end = (500, 200, numpy.pi/2)
-
-	HybridAStar(terrain, start, end)
+	start = HybridNode(500, 800, 0, 0)
+	end = HybridNode(500, 200, numpy.pi/2, 0)
+	
+	plan = HybridAStar(terrain, start, end)
--- A_Star/hybridNode.py
+++ A_Star/hybridNode.py
@@ -2,7 +2,7 @@ import numpy
 import cv2
 
 class HybridNode:
-	def __init__(self, x, y, th, definition=40, ):
+	def __init__(self, x, y, th, cost, definition=200):
 		self.x = x
 		self.y = y
 		self.th = th
@@ -10,6 +10,12 @@ class HybridNode:
 
 		self.parent = None
 		self.children = []
+		self.path = None
+		self.cost = 0
+		self.backwards = True
+
+	def __str__(self):
+		return "{0}x, {1}y, {2}th, {3}c".format(self.x, self.y, self.th, self.cost)
 
 	def addParent(self, par):
 		self.parent = par 
@@ -23,56 +29,86 @@ class HybridNode:
 	def getChildren(self):
 		return self.children
 
-	def extend(self, speed, phi):
+	def extendPath(self, speed, phi):
 		midways = []
 		jump = speed/self.definition
+		dth = speed*numpy.tan(phi)
 		curr_th = self.th
 		x = self.x
 		y = self.y
 		for i in range(self.definition):
 			x = jump*numpy.cos(curr_th) + x
 			y = jump*numpy.sin(curr_th) + y
-			curr_th = curr_th + phi
+			curr_th = curr_th + dth
 			midways.append((x, y, curr_th))
-		return midways
 
-	def getNeighbors(self, speed, phiMax, num):
+		newNode = HybridNode(x, y, curr_th, self.cost+speed)
+		newNode.path = midways
+
+		return newNode
+
+	def getNeighbors(self, speed, maxRadius, num):
+		phiMax = numpy.arcsin(1/(maxRadius*25))
 		dt = phiMax*2/(num-1)
 		curr_phi = -phiMax
-		results = ([], [])
+		self.children = []
+
+		#forwards propogation
 		for i in range(num):
-			curr_path = self.extend(speed, curr_phi)
-			tx, ty, tth = curr_path[-1]
-			results[0].append(HybridNode(tx, ty, tth))
-			results[1].append(curr_path)
+			self.children.append(self.extendPath(speed, curr_phi))
 			curr_phi += dt
-		return results
+
+		#allow backwards propogation
+		if self.backwards:
+			curr_phi = -phiMax
+			for i in range(num):
+				self.children.append(self.extendPath(-speed, curr_phi))
+				curr_phi += dt
+
+		return self.children
+
+	def distTo(self, other):
+		dx = self.x - other.x
+		dy = self.y - other.y
+		dt = self.th - other.th
+		return numpy.sqrt(dt**2 + dx**2 + dy**2)
 
 
 if __name__ == "__main__":
 	cv2.namedWindow("test", cv2.WINDOW_NORMAL)
 	#fix orientation
-	x, y, th = 250, 150, numpy.pi
-	n = HybridNode(x, y, th)
-	terrain = numpy.zeros((300, 300, 3))
+	x, y, th = 500, 500, numpy.pi*2/4
+	n = HybridNode(x, y, th, 0)
+	terrain = numpy.zeros((1000, 1000, 3))
 	terrain[:,:] = (255, 255, 255)
 	#cv2.imshow("test", terrain)
 	#cv2.waitKey(0)
 	colors = [(0, 0, 0), (0, 0, 255), (0, 255, 255), (0, 255, 0), (255, 255, 0), (255, 0, 0), (255, 0, 255)]
 	
-	cv2.circle(terrain, (x, y), 1, (0,255,0))
 	nodes = [n]
+	neighbors = 3
 
-	for i in range(343):
+	for i in range(1 + 2*neighbors + (2*neighbors)**2):
 		curr_colr = colors[i%7]
 		curr_node = nodes.pop(0)
-		extension = curr_node.getNeighbors(40, 0.02, 7)
-		nodes.extend(extension[0])
+		extension = curr_node.getNeighbors(100, 600, neighbors)
+		nodes.extend(extension)
 
-		for path in extension[1]:
-			for dot in path:
-				terrain[int(dot[0]), int(dot[1])] = curr_colr
+		for node in extension:
+			for dot in node.path:
+				x, y = int(dot[0]), 1000-int(dot[1])
+				if x>=1000 or x<0 or y>=1000 or y<0:
+					continue
+				terrain[1000-int(dot[1]), int(dot[0])] = curr_colr
+
+	cv2.circle(terrain, (x, y), 1, (0,255,0))
 
 	cv2.imshow("test", terrain)
 	cv2.waitKey(0)
+
+'''
+1600 -> 422 484   62
+800  -> 453 484   31
+400  -> 467 484   17
+'''
 
